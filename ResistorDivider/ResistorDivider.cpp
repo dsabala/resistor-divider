896966774e43598bfb0ff811fc125d331b597b43
Dariusz Sabała <>
2020-09-07 21:32:17 +0200

    -initial commit

---
+++ ResistorDivider/ResistorDivider.cpp
@@ -0,0 +1,12 @@
+﻿// ResistorDivider.cpp: definiuje punkt wejścia dla aplikacji.
+//
+
+#include "ResistorDivider.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
