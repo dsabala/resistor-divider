394372485dd7aff6f18c5b6a58c9ff1c955ec6cf
dsabala <70911794+dsabala@users.noreply.github.com>
2020-12-27 21:33:47 +0100

    fixed typo

--- cmake/toolchains/MinGW.cmake
+++ cmake/toolchains/MinGW.cmake
@@ -3,4 +3,6 @@ SET(CMAKE_SYSTEM_NAME Windows)
 
 # which compilers to use for C and C++
 SET(CMAKE_C_COMPILER gcc)
-SET(CMAKE_CXX_COMPILERg++)
+SET(CMAKE_CXX_COMPILER g++)
+
+#TODO intellisense dont see standard library headers 
