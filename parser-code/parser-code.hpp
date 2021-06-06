de05039618a277f077e70a835eb6aa166681d383
dsabala <70911794+dsabala@users.noreply.github.com>
2021-06-06 22:37:42 +0200

    formated and changed style

--- parser-code/parser-code.hpp
+++ parser-code/parser-code.hpp
@@ -1,30 +1,17 @@
 /**
- * Project: Resistor Divider
- * Description: Simple program that helps you look for pairs of resistors for voltage dividers. 
- * Author: Dariusz Sabala
+ * Project: resistor-divider
+ * Site: https://github.com/dsabala/resistor-divider
  * License: BSD
  */
 
 #pragma once
 
-#include <regex>
-#include <cassert>
-#include <cmath>
-
-namespace resistor_divider {
-
-namespace code_parser {
+namespace parser_code {
 
 int FindPrefix(std::string const code);
-
 int FindSuffix(std::string const code);
-
 int FindMul(std::string const code);
-
 int FindSuffixDiv(std::string const code);
-
 std::uint64_t ParseCodeTo_mR(std::string const code);
 
-}
-
-}
+}  // namespace parser_code
--- parser-code/tests/test-parser-code.cpp
+++ parser-code/tests/test-parser-code.cpp
@@ -1,73 +1,77 @@
+/**
+ * Project: resistor-divider
+ * Site: https://github.com/dsabala/resistor-divider
+ * License: BSD
+ */
+
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
 #include<doctest/doctest.h>
 #include<parser-code.hpp>
 
-namespace res_parser = resistor_divider::code_parser;
-
 TEST_CASE("Find resistor code prefix") {
-    CHECK(res_parser::FindPrefix("1k5") == 1);
-    CHECK(res_parser::FindPrefix("9k5") == 9);
-    CHECK(res_parser::FindPrefix("99k5") == 99);
-    CHECK(res_parser::FindPrefix("470k") == 470);
+    CHECK(parser_code::FindPrefix("1k5") == 1);
+    CHECK(parser_code::FindPrefix("9k5") == 9);
+    CHECK(parser_code::FindPrefix("99k5") == 99);
+    CHECK(parser_code::FindPrefix("470k") == 470);
 
-    CHECK(res_parser::FindPrefix(" 9k5") == 9);
-    CHECK(res_parser::FindPrefix("9k5 ") == 9);
-    CHECK(res_parser::FindPrefix(" 9k5 ") == 9);
+    CHECK(parser_code::FindPrefix(" 9k5") == 9);
+    CHECK(parser_code::FindPrefix("9k5 ") == 9);
+    CHECK(parser_code::FindPrefix(" 9k5 ") == 9);
 
-    CHECK(res_parser::FindPrefix("0k47") == 0);
-    CHECK(res_parser::FindPrefix("k47") == 0);
+    CHECK(parser_code::FindPrefix("0k47") == 0);
+    CHECK(parser_code::FindPrefix("k47") == 0);
 }
 
 TEST_CASE("Ensure other letter than k are discovered too") {
-    CHECK(res_parser::FindPrefix("1R5") == 1);
-    CHECK(res_parser::FindPrefix("9M5") == 9);
-    CHECK(res_parser::FindPrefix("0R47") == 0);
-    CHECK(res_parser::FindPrefix("M47") == 0);
+    CHECK(parser_code::FindPrefix("1R5") == 1);
+    CHECK(parser_code::FindPrefix("9M5") == 9);
+    CHECK(parser_code::FindPrefix("0R47") == 0);
+    CHECK(parser_code::FindPrefix("M47") == 0);
 }
 
 TEST_CASE("Find resistor code suffix") {
-    CHECK(res_parser::FindSuffix("1k1") == 1);
-    CHECK(res_parser::FindSuffix("1k9") == 9);
-    CHECK(res_parser::FindSuffix("1k99") == 99);
+    CHECK(parser_code::FindSuffix("1k1") == 1);
+    CHECK(parser_code::FindSuffix("1k9") == 9);
+    CHECK(parser_code::FindSuffix("1k99") == 99);
 
-    CHECK(res_parser::FindSuffix(" 9k5") == 5);
-    CHECK(res_parser::FindSuffix("9k5 ") == 5);
-    CHECK(res_parser::FindSuffix(" 9k5 ") == 5);
+    CHECK(parser_code::FindSuffix(" 9k5") == 5);
+    CHECK(parser_code::FindSuffix("9k5 ") == 5);
+    CHECK(parser_code::FindSuffix(" 9k5 ") == 5);
 
-    CHECK(res_parser::FindSuffix("0k47") == 47);
-    CHECK(res_parser::FindSuffix("k47") == 47);
+    CHECK(parser_code::FindSuffix("0k47") == 47);
+    CHECK(parser_code::FindSuffix("k47") == 47);
 }
 
 TEST_CASE("Find multiplier") {
-    CHECK(res_parser::FindMul("1R1") == 1'000);
-    CHECK(res_parser::FindMul("1k1") == 1'000'000);
-    CHECK(res_parser::FindMul("1M1") == 1'000'000'000);
+    CHECK(parser_code::FindMul("1R1") == 1'000);
+    CHECK(parser_code::FindMul("1k1") == 1'000'000);
+    CHECK(parser_code::FindMul("1M1") == 1'000'000'000);
 }
 
 TEST_CASE("Find suffix divider") {
-    CHECK(res_parser::FindSuffixDiv("1k1") == 10);
-    CHECK(res_parser::FindSuffixDiv("3k9") == 10);
-    CHECK(res_parser::FindSuffixDiv("1k05") == 100);
-    CHECK(res_parser::FindSuffixDiv("1k99") == 100);
+    CHECK(parser_code::FindSuffixDiv("1k1") == 10);
+    CHECK(parser_code::FindSuffixDiv("3k9") == 10);
+    CHECK(parser_code::FindSuffixDiv("1k05") == 100);
+    CHECK(parser_code::FindSuffixDiv("1k99") == 100);
 }
 
 TEST_CASE("Finally parse the resistore code") {
-    CHECK(res_parser::ParseCodeTo_mR("0R01")  ==              10);
-    CHECK(res_parser::ParseCodeTo_mR("0R1")   ==             100);
-    CHECK(res_parser::ParseCodeTo_mR("0R47")  ==             470);
-    CHECK(res_parser::ParseCodeTo_mR("R47")   ==             470);
-    CHECK(res_parser::ParseCodeTo_mR("220R")  ==         220'000);
-    CHECK(res_parser::ParseCodeTo_mR("k22")   ==         220'000);
-    CHECK(res_parser::ParseCodeTo_mR("1k")    ==       1'000'000);
-    CHECK(res_parser::ParseCodeTo_mR("1k05")  ==       1'050'000);
-    CHECK(res_parser::ParseCodeTo_mR("1k0")   ==       1'000'000);
-    CHECK(res_parser::ParseCodeTo_mR("1k12")  ==       1'120'000);
-    CHECK(res_parser::ParseCodeTo_mR("47k")   ==      47'000'000);
-    CHECK(res_parser::ParseCodeTo_mR("47k2")  ==      47'200'000);
-    CHECK(res_parser::ParseCodeTo_mR("470k")  ==     470'000'000);
-    CHECK(res_parser::ParseCodeTo_mR("1M0")   ==   1'000'000'000);
-    CHECK(res_parser::ParseCodeTo_mR("1M2")   ==   1'200'000'000);
-    CHECK(res_parser::ParseCodeTo_mR("1M22")  ==   1'220'000'000);
-    CHECK(res_parser::ParseCodeTo_mR("470M0") == 470'000'000'000);
+    CHECK(parser_code::ParseCodeTo_mR("0R01")  ==              10);
+    CHECK(parser_code::ParseCodeTo_mR("0R1")   ==             100);
+    CHECK(parser_code::ParseCodeTo_mR("0R47")  ==             470);
+    CHECK(parser_code::ParseCodeTo_mR("R47")   ==             470);
+    CHECK(parser_code::ParseCodeTo_mR("220R")  ==         220'000);
+    CHECK(parser_code::ParseCodeTo_mR("k22")   ==         220'000);
+    CHECK(parser_code::ParseCodeTo_mR("1k")    ==       1'000'000);
+    CHECK(parser_code::ParseCodeTo_mR("1k05")  ==       1'050'000);
+    CHECK(parser_code::ParseCodeTo_mR("1k0")   ==       1'000'000);
+    CHECK(parser_code::ParseCodeTo_mR("1k12")  ==       1'120'000);
+    CHECK(parser_code::ParseCodeTo_mR("47k")   ==      47'000'000);
+    CHECK(parser_code::ParseCodeTo_mR("47k2")  ==      47'200'000);
+    CHECK(parser_code::ParseCodeTo_mR("470k")  ==     470'000'000);
+    CHECK(parser_code::ParseCodeTo_mR("1M0")   ==   1'000'000'000);
+    CHECK(parser_code::ParseCodeTo_mR("1M2")   ==   1'200'000'000);
+    CHECK(parser_code::ParseCodeTo_mR("1M22")  ==   1'220'000'000);
+    CHECK(parser_code::ParseCodeTo_mR("470M0") == 470'000'000'000);
 }
