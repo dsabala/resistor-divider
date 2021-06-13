1206949d36244eb284a67f6d57c27973561871af
dsabala <70911794+dsabala@users.noreply.github.com>
2021-06-13 21:48:22 +0200

    first unit test of pairs list generator

--- resistor-divider-logic/tests/test_pairs_finder.cpp
+++ resistor-divider-logic/tests/test_pairs_finder.cpp
@@ -14,7 +14,26 @@
 TEST_CASE("Pairs list generation") {
   const std::vector<std::uint64_t> val_multipliers{10, 100};
   const std::vector<std::uint64_t> val_serie2{22, 47};
-  std::vector<ResistorsPair> ratio_list_expected{};
+  // clang-format off
+  std::vector<ResistorsPair> ratio_list_expected{
+    {220,  4700},
+    {220,  2200},
+    {470,  4700},
+    {470,  2200},
+    {220,  470},
+    {2200, 4700},
+    {220,  220},
+    {470,  470},
+    {2200, 2200},
+    {4700, 4700},
+    {470,  220},
+    {4700, 2200},
+    {2200, 470},
+    {2200, 220},
+    {4700, 470},
+    {4700, 220}
+  };
+  // clang-format on
   std::vector<ResistorsPair> ratio_list_gen;
   PairsFinder::GenerateRatioList(ratio_list_gen, val_multipliers, val_serie2);
   CHECK(ratio_list_expected == ratio_list_gen);
