417c224a8f5c849d13172fd3eba98146d7cd2c3d
dsabala <70911794+dsabala@users.noreply.github.com>
2021-06-28 00:59:30 +0200

    just testing jenkins badge

--- resistor-divider-logic/tests/test_resistor_pair.cpp
+++ resistor-divider-logic/tests/test_resistor_pair.cpp
@@ -14,7 +14,7 @@ TEST_CASE("Resistor class constructor default") {
 
 TEST_CASE("Resistor class constructor numeric") {
   Resistor resistor(500);
-  CHECK(resistor.GetValue() == 500);
+  CHECK(resistor.GetValue() == 400);
 }
 
 TEST_CASE("Resistor class value setter") {
